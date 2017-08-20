dadeaa7211015b251195eb7b6b5e762a33241acd
CJBLUE <18341911386@163.com>
2017-08-20 14:59:00 +0800

    Initial Commit

---
+++ CJLightViewContriller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJLightViewContriller
+//
+//  Created by zxjk on 17/8/20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJLightViewContriller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJLightViewContriller
+//
+//  Created by zxjk on 17/8/20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
